a2188e06be02a6abb650469b8633ecc9c299b824
ayushnvs <ayush.mishra.ald@gmail.com>
2022-09-23 21:58:10 +0530

    adding script

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,10 +1,35 @@
 let browserInnerHeight = window.innerHeight;
 
-function getElementNode(selector = {}) {
+function getElementNode(selector) {
 	if (selector.css) {
-		let elNOde = document.querySelector(selector.css)
+		let elNode = document.querySelector(selector.css)
+        elNode = elNode ? elNode : null
 	}
 	if (selector.xpath) {
-		elNOde = document.evaluate(selector.xpath, null, document, )
+		elNode = document.evaluate(selector.xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        elNode = elNode.singleNodeValue ? elNode.singleNodeValue : null
 	}
+    return elNode
+}
+
+function scrolltoView(selector)  {
+    let node = getElementNode(selector)
+    if (node) {
+        node.scrollIntoView()
+    }
+    else { console.log("Element with given selector is not present!") }
+}
+
+function addInnerText(selector, text) {
+    let node = getElementNode(selector)
+    if (node) {
+        node.textContent = text
+    }
+    else { console.log("Element with given selector is not present!") }
+}
+
+function getwindowHW() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    return {"height": height, "width": width}
 }
