3fd65dcb5a1c77ffa3e685d0f3fee9608e0db605
Ayush Mishra <73516326+ayushnvs@users.noreply.github.com>
2022-10-09 10:42:24 +0000

    added username in about, community and contact pages

--- pages/views.py
+++ pages/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render, redirect
 def home_view(request, *args, **kwargs):
     if request.user.is_authenticated:
         return redirect(f'/{request.user.username}')
-    return render(request, 'sargam/home.html', {})
+    return render(request, 'sargam/home.html', {"username": request.user.username})
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, 'sargam/about.html', {})
+    return render(request, 'sargam/about.html', {"username": request.user.username})
 
 
 def community_view(request, *args, **kwargs):
-    return render(request, 'sargam/community.html', {})
+    return render(request, 'sargam/community.html', {"username": request.user.username})
 
 
 def contact_view(request, *args, **kwargs):
-    return render(request, 'sargam/contact.html', {})
+    return render(request, 'sargam/contact.html', {"username": request.user.username})
