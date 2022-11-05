327fd64c9daa187d5e5b3aed4a14b2488ceffc67
ayushnvs <ayush.mishra.ald@gmail.com>
2022-11-05 21:43:44 +0530

    updated script to use jshelpermodule

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,35 +1,3 @@
-let browserInnerHeight = window.innerHeight;
+import { jsHelper } from "../../JSHelper/helper";
 
-function getElementNode(selector) {
-	if (selector.css) {
-		let elNode = document.querySelector(selector.css)
-        elNode = elNode ? elNode : null
-	}
-	if (selector.xpath) {
-		elNode = document.evaluate(selector.xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        elNode = elNode.singleNodeValue ? elNode.singleNodeValue : null
-	}
-    return elNode
-}
-
-function scrolltoView(selector)  {
-    let node = getElementNode(selector)
-    if (node) {
-        node.scrollIntoView()
-    }
-    else { console.log("Element with given selector is not present!") }
-}
-
-function addInnerText(selector, text) {
-    let node = getElementNode(selector)
-    if (node) {
-        node.textContent = text
-    }
-    else { console.log("Element with given selector is not present!") }
-}
-
-function getwindowHW() {
-    let height = window.innerHeight
-    let width = window.innerWidth
-    return {"height": height, "width": width}
-}
+const helper = new jsHelper()
