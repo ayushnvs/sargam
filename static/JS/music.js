82bd981275453ede826cdcc2b7bb6560d58076f9
ayushnvs <ayush.mishra.ald@gmail.com>
2023-02-04 20:03:32 +0530

    modified and added music files

---
+++ static/JS/music.js
@@ -0,0 +1,11 @@
+const music = document.querySelector('#audio')
+
+const musicBanner = document.querySelector('div.msc-banner img')
+const musicName = document.querySelector('div.msc-name p')
+const seekBar = document.querySelector('input.seek-bar')
+const currentTime = document.querySelector('span.current-time')
+const songDuration = document.querySelector('span.song-duration')
+const forwardBtn = document.querySelector('button.fb-btn.forward')
+const backwardBtn = document.querySelector('button.fb-btn.backward')
+const playBtn = document.querySelector('button.play-button')
+
